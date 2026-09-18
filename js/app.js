e4ae9aa9452f50533feb78073df3272618aaf0aa
Mahender <mahender1100@gmail.comL>
2026-09-18 07:10:57 +0530

    refactor: remove "My reading time" feature for cleaner codebase

--- js/app.js
+++ js/app.js
@@ -591,13 +591,6 @@ function renderContentLayer() {
                 <div class="rich-content" id="rc-explanation"></div>
             </div>
 
-            ${hasContent ? `
-                <div class="my-reading-time-row">
-                    <button type="button" id="my-reading-time-btn" class="my-reading-time-btn">
-                        My reading time
-                    </button>
-                </div>` : ""}
-
             ${diagrams.length ? `
                 <div class="topic-section">
                     <h3>Diagrams / Graphs</h3>
